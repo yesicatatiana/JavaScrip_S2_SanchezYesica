046eea8d0aff0053183f900d54cd5828c01226f9
yesica <ytatiana865@gmail.com>
2025-07-18 12:22:23 -0500

    Feat: :zap: js

---
+++ Dia1/scripts/script.js
@@ -0,0 +1,47 @@
+// #####################
+// ###### Dia 1 ########
+// #####################
+
+//¡¡¡Primer comando!!!
+
+//Number
+consoles.log("hola mundo");
+variale1= 3.14;
+console.log(variable1);
+console.log(typeof (variable1));
+
+//Concatenacion
+palabra1="campus";
+palabra2="lannds";
+palabra3=palabra1+palabra2;
+console.log(palabra3);
+
+nombrePersona="luis angel";
+plataActual=55000;
+console.log("la persona llamada",nombrePersona,"tiene en su cuenta bancaria",plataActual,"pesos colombianos!");
+
+//Booleanos
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1);
+
+//Condicionales
+if(boleanito1 == true){
+    consoles.log("Es verdadero!!");
+}else{
+    console.log("No es verdadero : sadfeis:");
+}
+
+//Bucles
+for(i=0;i<4;i+=i+1){
+    console.log("wenas");
+}
+
+holi = true;
+while(holi==true){
+    console.log(wenas);
+    holi=false;
+}
+
+
+// Desarrollado por: Yesica Sanchez - c.c: 1.095.304.945
