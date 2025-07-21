16b1740bef0185ff893735c5c125ecefc349548c
yesica <ytatiana865@gmail.com>
2025-07-21 12:58:48 -0500

    Feat: :hammer: advance

---
+++ Dia1.1/scripts/script.js
@@ -0,0 +1,29 @@
+
+let edad;
+edad = prompt('Ingrese su edad:')
+console.log(typeof edad);
+
+// #####################
+// #### Tarea Dia 1 ####
+// #####################
+
+
+let booleanito = true;
+
+while (booleanito){
+    let opcion = prompt(`
+    =============================================
+                  Menu Principal                
+    =============================================
+    Seleccione una opción:
+    1. Registrar nuevo gasto
+    2. Listar gastos
+    3. Calcular total de gastos
+    4. Generar reporte de gastos
+    5. Salir
+    =============================================` );
+
+    if (opcion === 1){
+        
+    }
+}
