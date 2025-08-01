5668b14428d92b5c03076d205dae64220e54b911
yesica <ytatiana865@gmail.com>
2025-07-31 21:53:57 -0500

    feat :sparkles: API

--- Dia8/scripts/scripts.js
+++ Dia8/scripts/scripts.js
@@ -2,17 +2,24 @@ function buscarPersonaje (){
     const nombrePersonaje = prompt("ingresa el ID del personaje");
     const xhr = new XMLHttpRequest ();
 
-    const url = `https://swapi.py4e.com/api/`;
+    const url = `https://swapi.py4e.com/api/?:id=${nombrePersonaje}`;
 
-    xhr.open("GET", url, true)
+    xhr.open("GET", url, true);
 
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
+            try{
+
             const datos = JSON.parse(xhr.responseText);
-            alert("The character is: name:" + datos["results"][0]["name"]);
+                alert("The character is: name:" + datos["results"][0]["name"]);
+            }
+            catch(err){
+                console.log(err.message);
         }
     }
+    xhr.send();
 
 
-
+    }
 }
+buscarPersonaje();
